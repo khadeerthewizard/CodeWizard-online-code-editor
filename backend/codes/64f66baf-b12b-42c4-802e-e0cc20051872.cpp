9ddf62774732ca91b3ca50047c9ee73eb8962d1f
khadeer <khadeer2204@gmail.com>
2024-03-30 19:15:34 +0530

    Updates

--- backend/codes/64f66baf-b12b-42c4-802e-e0cc20051872.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int** SudokuSolver(int** board, int boardSize, int** solved) {
-  for (int i = 0; i < boardSize; i++) {
-    for (int j = 0; j < boardSize; j++) { printf("%d ", board[i][j]); }
-    printf("\n");
-  }
-  return board;
-}
