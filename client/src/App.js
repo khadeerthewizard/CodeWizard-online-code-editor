e0759220620e10bb694e2b66a22012438e36405b
khadeer <khadeer2204@gmail.com>
2025-02-09 14:27:32 +0530

    Minor fix

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ import TextareaAutosize from '@mui/base/TextareaAutosize';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 require('dotenv').config()
-const bapiUrl = process.env.bapiUrl;
 function App() {
   const [code, setCode] = useState('');
   const [output, setOutput] = useState('');
@@ -27,8 +26,6 @@ function App() {
     console.log(code);
     try {
       axios.post(bapiUrl+"/run",payload)
-      axios.post(bapiUrl,payload)
-      main
     .then((response) => {
       console.log(response);
       setOutput(response.data.output);
